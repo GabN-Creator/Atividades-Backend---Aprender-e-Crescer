2072c6cca875d6b35524223e7a7f82d56f8cea80
gabizoca <noaragabis@gmail.com>
2026-08-26 22:25:44 -0300

    criando novas rotas

--- minha-primeiraAPI/index.js
+++ minha-primeiraAPI/index.js
@@ -1,1 +1,14 @@
-setImmediate
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('seja bem-vindo a gestão de livros');
+});
+
+app.get('/livros', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000);
+
